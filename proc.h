49a44485ae971a11ba40b7740291dee46f0b78e8
kawaiiandgothic <67576969+kawaiiandgothic@users.noreply.github.com>
2023-12-22 08:53:15 -0600

    Update proc.h

--- proc.h
+++ proc.h
@@ -6,5 +6,5 @@ void* proc_handle;
 unsigned long proc_id;
 
 int proc_init(const char* window_name);
-size_t proc_module(wchar_t* module_name);
+size_t proc_module(const wchar_t* module_name);
 void* proc_read(void* address);
